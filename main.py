c0cd7896b83dedf42cb4e361f350dbb741264c7e
sota.m <sota.m777121@gmail.com>
2021-07-06 15:02:17 +0900

    Update gitignore, remove trailing white spaces in main.py

--- main.py
+++ main.py
@@ -33,14 +33,14 @@ def download_conversations_list(client, page_limit: int) -> List[dict]:
     while next_obj_exists is True:
         slack_response = client.conversations_list(
                             cursor = next_cursor,
-                            limit = page_limit, 
+                            limit = page_limit,
                             types = 'public_channel,private_channel')
-        
+
         channels.extend(slack_response.get('channels'))
         next_cursor = slack_response.get('response_metadata').get('next_cursor')
         if next_cursor == "":
             next_obj_exists = False
-    
+
     return channels
 
 
@@ -56,12 +56,12 @@ def download_users_list(client, page_limit: int) -> List[dict]:
         slack_response = client.users_list(
                             cursor = next_cursor,
                             limit = page_limit)
-        
+
         users.extend(slack_response.get('members'))
         next_cursor = slack_response.get('response_metadata').get('next_cursor')
         if next_cursor == "":
             next_obj_exists = False
-    
+
     return users
 
 
@@ -96,7 +96,7 @@ def download_conversations_history(
             break
         if next_cursor == "":
             next_obj_exists = False
-    
+
     return conversations_by_channel
 
 
@@ -148,7 +148,7 @@ def ingest_slack_data(request, **kwargs):
         token=os.environ.get("SLACK_BOT_TOKEN"),
         signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
     )
-    
+
     # Settings
     _conditions = {}
     if request is None: # locally test
@@ -160,7 +160,7 @@ def ingest_slack_data(request, **kwargs):
     latest_unix_time = _conditions['latest_ut'] if ('latest_ut' in _conditions.keys()) else None
     oldest_unix_time = _conditions['oldest_ut'] if ('oldest_ut' in _conditions.keys()) else None
     bucket_name = _conditions['bucket_name'] if ('bucket_name' in _conditions.keys()) else None
-    
+
     # 時刻が明示されていない場合は、通常のデイリー実行を前提として
     # データ取得期間を定義する
     if latest_unix_time is None or oldest_unix_time is None:
@@ -172,17 +172,17 @@ def ingest_slack_data(request, **kwargs):
     out_dir = exporting_dir(oldest_ut=oldest_unix_time)
     logging.info('out_dir : {}'.format(out_dir))
     logging.info('oldest_ut : {}'.format(oldest_unix_time) + ' | latest_ut : {}'.format(latest_unix_time))
-    
+
     client = app.client
-    
+
     # ingest channles list
     channels = download_conversations_list(client=client, page_limit=100)
     save_as_json(channels, out_dir + '/' + 'conversations_list.json')
-    
+
     # ingest users list
     users = download_users_list(client=client, page_limit=100)
     save_as_json(users, out_dir + '/' + 'users_list.json')
-    
+
     # ingest conversations history
     channel_id_list, channel_name_list = target_channel_id_name_list(channels, including_archived=False)
     conversations = []
@@ -195,20 +195,20 @@ def ingest_slack_data(request, **kwargs):
         if len(conversations_by_ch) > 0:
             conversations.extend(conversations_by_ch)
     save_as_json(conversations, out_dir + '/' + 'conversations_history.json')
-    
+
     # # save completing log
     # tz = pytz.timezone('Asia/Tokyo')
     # now = datetime.datetime.now(tz)
     # ingest_log = {'ingested_at_ts': now.timestamp(), 'ingested_at': now.strftime('%Y-%m-%d %H:%M:%S')}
     # save_into_bucket(ingest_log, bucket, out_dir + '/' + 'ingest_log.json')
-    
+
     # == only local env ==
     # copy log to export dir
     from_log_path = Path(logfilename)
     to_log_path = Path(out_dir)
     shutil.copy2(from_log_path, to_log_path)
     # == only local env ==
-    
+
     return f"Successfully ingested slack data."
 # ==  END - Main Cloud Function  ==
 
